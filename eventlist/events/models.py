4e87ca3921a226ee3ab75f70dfa2882998187017
th0 <th0@wkurwia.pl>
2015-08-28 14:30:55 +0200

    added simple event models and serializer

--- eventlist/events/models.py
+++ eventlist/events/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+# Simplified Event model
+class Event(models.Model):
+  calendarId = models.CharField(max_length=255),
+  eventId = models.CharField(max_length=255),
+  obtained = models.DateTimeField(auto_now_add=True),
